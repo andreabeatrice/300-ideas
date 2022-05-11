d1ba67fa1ba6c98e6c0a14fb36f7ae48fcf4681b
andreabeatrice <86042775+andreabeatrice@users.noreply.github.com>
2022-05-11 13:50:12 +0200

    TextScrolls

---
+++ Workshop 3/Workshop3_powerups/Assets/Scripts/ClickCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickCounter : MonoBehaviour
+{
+
+    int counter = 0;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(100, 100, 200, 50), "COUNTER: " + counter);
+        {
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+            counter++;
+    }
+}
---
+++ Workshop3_ScrollingText/Assets/Autoscroll.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Autoscroll : MonoBehaviour
+{
+    float speed = 100.0f;
+    //float textPosBegin = -707.0f;
+    float boundaryTextEnd = 4044.0f;
+
+    RectTransform myGORectTransform;
+    [SerializeField]
+    TextMeshProUGUI mainText;
+
+   // [SerializeField]
+   // bool isLooping = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myGORectTransform = gameObject.GetComponent<RectTransform>();
+        StartCoroutine(AutoScrollText());
+
+    }
+
+    IEnumerator AutoScrollText()
+    {
+        while(myGORectTransform.localPosition.y < boundaryTextEnd)
+        {
+            myGORectTransform.Translate(Vector3.up * speed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            counter++;
+
+
+        if (counter >= 10)
+        {
+            speed = 40.0f;
+
+            StartCoroutine(SlowDownPowerUp());
+        }
+        
+
+        
+    }
+
+    IEnumerator SlowDownPowerUp()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        counter = 0;
+        speed = 100.0f;
+    }
+
+    int counter = 0;
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(100, 100, 200, 50), "COUNTER: " + counter);
+        {
+        }
+    }
+
+
+}
